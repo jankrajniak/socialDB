48a9cd392918f5235e9a6cf855738e5e36c5403e
jankrajniak <jan.krajniak@gmail.com>
2025-02-27 12:41:31 -0500

    reactions routes and controllers completed. Working on removing all associated thoughts for a deleted user next.

--- src/controllers/reactionController.ts
+++ src/controllers/reactionController.ts
@@ -1,9 +1,43 @@
 import { Response, Request } from 'express';
+import { Thought } from '../models/index.js';
 
 export const addReaction = async (req: Request, res: Response) => {
-    res.status(200).json({ message: `Will ADD a reaction to thought with ID ${req.params.thoughtId}` });
+    const { thoughtId } = req.params;
+    const { reactionBody } = req.body;
+
+    if (!thoughtId || !reactionBody) {
+        res.status(400).json({ message: 'Thought ID and reaction body are required' });
+    } else {
+        const updatedThought = await Thought.findByIdAndUpdate(
+            thoughtId,
+            { $push: { reactions: { reactionBody}}},
+            { new: true},
+        )
+
+        if (!updatedThought) {
+            res.status(404).json({ message: 'No thought found with this ID' });
+        } else {
+            res.status(200).json(updatedThought);
+        }
+    }
 };
 
 export const deleteReaction = async (req: Request, res: Response) => {
-    res.status(200).json({ message: `Will DELETE a reaction with ID ${req.params.reactionId} from thought with ID ${req.params.thoughtId}`});
+    const { thoughtId, reactionId } = req.params;
+
+    if (!thoughtId || !reactionId) {
+        res.status(400).json({ message: 'Thought ID and reaction ID are required' });
+    } else {
+        const updatedThought = await Thought.findByIdAndUpdate(
+            thoughtId,
+            { $pull: { reactions: { reactionId }}},
+            { new: true},
+        )
+
+        if (!updatedThought) {
+            res.status(404).json({ message: 'Thought or reaction not found' });
+        } else {
+            res.status(200).json(updatedThought);
+        }
+    }
 };
